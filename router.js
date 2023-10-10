f1b13e36a205feb37cf190da717ac48f6801f8eb
zinzinyou <zinzinyou@korea.ac.kr>
2023-10-10 11:12:26 +0900

    .

--- router.js
+++
@@ -1,7 +0,0 @@
-const {Router} = require('express');
-const router = Router();
-
-router.get('/',(req,res)=>res.send('GET /'));
-router.post('/',(req,res)=>res.send('POST /'));
-
-module.exports = router;
